afa14c81c47c779323bcabedf3f19342e3c0f763
thomasam000 <thomasam000@gmail.com>
2022-05-16 21:36:27 -0400

    changes to main page

--- src/pages/page-home.vue
+++ src/pages/page-home.vue
@@ -1,17 +1,16 @@
 <template>
 <div>
-    test
     <div v-if="dailyAgenda">
         <div v-for="item in dailyAgenda.dailyAgendaItems" :key="item.id">
             {{item.task.name}}: {{item.task.description}}
             <div v-if="item.task.type == 'boolean'">
                 <div>goal: {{item.task.goal}}</div>
-                <div>result: {{item.result}}</div>
+                <input type="checkbox" v-model="item.result">
                 <div v-if="item.result"> Complete </div>
             </div>
             <div v-if="item.task.type == 'minutes'">
                 <div>goal: {{item.task.goal}}</div>
-                <div>result: {{item.result}}</div>
+                <input type="number" v-model="item.result">
                 <div v-if="item.result >= item.task.goal"> Complete </div>
             </div>
         </div>
@@ -20,52 +19,49 @@
 </template>
 
 <script>
-// import { mapActions } from 'vuex'
 
 export default {
     data () {
         return {
+            dailyAgenda: null,
             today: Math.floor(Date.now() / 1000)
         }
     },
     computed: {
-        dailyAgenda() {
-            let agendaObject = this.$store.state.dailyAgenda.find(agenda => 
-                new Date(agenda.date).getFullYear() === new Date(this.today).getFullYear() &&
-                new Date(agenda.date).getMonth() === new Date(this.today).getMonth() &&
-                new Date(agenda.date).getDate() === new Date(this.today).getDate()
-            )
-            if (agendaObject != undefined) {
-                agendaObject.dailyAgendaItems.forEach(item => {
-                    item.task = this.$store.state.tasks.find(task => task.id === item.taskID)
-                });
-            }
-            return agendaObject
-        },
 
     },
     methods: {
-        // ...mapActions('threads', ['createThread']),
+
     },
-    created() {
-            let agendaObject = this.$store.state.dailyAgenda.find(agenda => 
-                new Date(agenda.date).getFullYear() === new Date(this.today).getFullYear() &&
-                new Date(agenda.date).getMonth() === new Date(this.today).getMonth() &&
-                new Date(agenda.date).getDate() === new Date(this.today).getDate()
+    async created() {
+
+
+        let agendaObject = this.$store.state.dailyAgenda.find(agenda => 
+            new Date(agenda.date * 1000).getFullYear() === new Date(this.today * 1000).getFullYear() &&
+            new Date(agenda.date * 1000).getMonth() === new Date(this.today * 1000).getMonth() &&
+            new Date(agenda.date * 1000).getDate() === new Date(this.today * 1000).getDate()
+        )
+        if (agendaObject === undefined) {
+            await this.$store.dispatch('createDailyAgenda')
+            agendaObject = this.$store.state.dailyAgenda.find(agenda => 
+                new Date(agenda.date * 1000).getFullYear() === new Date(this.today * 1000).getFullYear() &&
+                new Date(agenda.date * 1000).getMonth() === new Date(this.today * 1000).getMonth() &&
+                new Date(agenda.date * 1000).getDate() === new Date(this.today * 1000).getDate()
             )
-            if (agendaObject === undefined) {
-                this.$store.dispatch('createDailyAgenda')
-            }
+            agendaObject.dailyAgendaItems.forEach(item => {
+                item.task = this.$store.state.tasks.find(task => task.id === item.taskID)
+            });
+            this.dailyAgenda = {... agendaObject}
 
-        // let ms = Math.floor(Date.now() / 1000);
-        // console.log(ms)
-        // this.today = ms
-        // console.log(ms);
-        // let date_string = new Date (1594122308).toDateString()
-        // console.log(date_string)
+        } else {
+
+            agendaObject.dailyAgendaItems.forEach(item => {
+                item.task = this.$store.state.tasks.find(task => task.id === item.taskID)
+            });
+            this.dailyAgenda = {... agendaObject}
+
+        }
     }
-
-    
 }
 </script>
 
